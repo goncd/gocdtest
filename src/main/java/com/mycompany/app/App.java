7a6269faa8ef2446be2bc7262f8e47ab3f67f1d3
goncd <27208645+goncd@users.noreply.github.com>
2025-01-10 11:30:21 +0100

    Añadir pipeline y programa de ejemplo

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,10 @@
+package com.mycompany.app;
+
+/**
+ * Ejemplo de programa en Java
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Programa generado por GoCD. Virtualización de Sistemas, curso 2024-2025.");
+    }
+}
---
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,19 @@
+package com.mycompany.app;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests de ejemplo.
+ */
+public class AppTest {
+
+    /**
+     * Ejemplo de test
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
